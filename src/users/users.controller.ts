23ae19a00ed936f2a899c9d46e0c44106c71bb89
고수희 <dev.suhee@gmail.com>
2022-12-17 18:30:45 +0900

    modify : /users/:id API exception

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -19,6 +28,9 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
+    if (+id < 1) {
+      throw new BadRequestException('Invalid Parameter');
+    }
     return this.usersService.findOne(+id);
   }
 
